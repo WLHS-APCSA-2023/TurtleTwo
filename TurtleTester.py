f124d9860f4f73080110984ee021a3575f4aefc9
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-12-14 13:53:05 -0800

    Create TurtleTester.py

---
+++ TurtleTester.py
@@ -0,0 +1,18 @@
+public class TurtleTester{
+	public static void main(String[] args) {
+		//instantiate a new turtle
+		
+    //square
+
+
+		//rectangle
+
+		//triangle
+
+
+		//circle
+
+
+
+	}
+}
